42c812b84882f74153de39c98353d98f21a0f8fa
github username <github email address>
2019-03-08 20:54:29 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+let body = document.querySelector("body");
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerText = "is the champion";
+console.log(newHeader.innerHTML === "is the champion");
+body.appendChild(newHeader);
