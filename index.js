c573e751b57c9c8fd2ed77cbf5516df680fc5f6e
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-03-10 11:55:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.className = "victory";
+newHeader.innerText = "Matt Brophy is the champion!";
