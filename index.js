9ca292c9496e700e15771dab24e7ec510eddd51a
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-03-10 15:37:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerText = "Lucy is the champion!";
