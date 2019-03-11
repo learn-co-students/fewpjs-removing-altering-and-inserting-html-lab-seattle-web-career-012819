dba8c915c01eea4a848e4de06817338b1c893cee
kwmcminn <kwmcminn@gmail.com>
2019-03-11 08:52:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.className = "victory";
+newHeader.innerText = "Kevin is the champion";
