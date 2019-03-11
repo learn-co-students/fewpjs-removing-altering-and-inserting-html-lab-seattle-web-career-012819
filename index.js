28dedcb6d99936678d6390f407be2be6b6fbf938
halented <hallie.dunn1@gmail.com>
2019-03-11 09:54:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// document.querySelector(body)
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerText = "here's a name is the champion";
