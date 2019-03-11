b1ef60037d248fd5ef472598753b60c30a400bab
elimerrell <elijah.merrell@gmail.com>
2019-03-11 09:03:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.className = 'victory';
+newHeader.innerText = 'Eli Merrell is the champion';
